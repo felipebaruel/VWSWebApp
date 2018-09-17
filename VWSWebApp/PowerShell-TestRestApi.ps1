93d5d5c9d375736c76284c36773ad3d04ec1bcf5
Victor Santana <vsantana@microsoft.com>
2018-09-16 20:33:28 -0500

    add powershell test file

---
+++ VWSWebApp/PowerShell-TestRestApi.ps1
@@ -0,0 +1,13 @@
+﻿$BodyStart = @'
+{
+    "Host":"www.google.com",
+    "Port":80,
+    "Connections":30,
+}
+'@
+
+Invoke-RestMethod -Uri http://localhost:54985/api/SocketStart -Method Post -Body $BodyStart -ContentType "application/json"
+
+Invoke-RestMethod -Uri http://localhost:54985/api/Socket -Method Get -ContentType "application/json"
+
+Invoke-RestMethod -Uri http://localhost:54985/api/SocketStop -Method Get -ContentType "application/json"
